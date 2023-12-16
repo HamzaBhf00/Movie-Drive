19d45b5f64a9064fb72b40c5802bf28d9ca8775f
HamzaBhf00 <alumno.1248865@ies-azarquiel.es>
2023-12-16 16:43:54 +0100

    fixing issues

--- backend/index.js
+++ backend/index.js
@@ -1,27 +1,34 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
-const cors = require('cors')
+const cors = require('cors');
+const fetch = require('node-fetch');
 
-app.use(cors())
+const corsOptions = {
+    origin: 'https://movie-drive.netlify.app/',
+    optionsSuccessStatus: 200,
+}
+
+app.use(cors(corsOptions));
 
 app.get('/', async (req, res) => {
     try {
-
         const id = process.env.ID;
         const response = await fetch(`https://drive.google.com/uc?id=${id}`);
 
-        const data = await response.json();
-        res.json(data);
-
-        console.log(data);
-
+        if (response.ok) {
+            const data = await response.json();
+            res.json(data);
+            console.log(data);
+        } else {
+            throw new Error('Failed to fetch data');
+        }
     } catch (error) {
         res.status(500).json({ error: error.message });
     }
 });
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000; 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
